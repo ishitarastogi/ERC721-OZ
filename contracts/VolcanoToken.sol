b66bc816de2aad8a3049e5a146211f7eaa7bdc65
ishitarastogi <Ishita3099@gmail.com>
2021-09-07 18:01:04 +0530

    Deployed contract to ganache

---
+++ contracts/VolcanoToken.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VolcanoToken is ERC721, Ownable {
+    constructor() ERC721("VolcanoToken", "VTC") {
+    }
+
+    uint256 tokenId;
+}
